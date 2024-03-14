9b4ebc64ffecab174b2874d2cc8f560e1592bdc6
macatnine <163325890+macatnine@users.noreply.github.com>
2024-03-14 22:33:42 +0100

    added naked web, jpa, h2

---
+++ src/main/java/com/example/demospringjpaschool/DemoSpringJpaSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringjpaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJpaSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJpaSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringjpaschool/DemoSpringJpaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringjpaschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJpaSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
